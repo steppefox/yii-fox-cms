bac50629e028ee04a0a2499ebd9d2c25cbd1fe0e
Eldar Amantay <amantay.eldar@gmail.com>
2013-08-06 00:57:37 +0600

    Delete old files

--- protected/views/site/pages/content.php
+++
@@ -1,6 +0,0 @@
-<div class='block <?php echo $model->alias; ?>'>
-                        
-	<h2><?php echo $model->title; ?></h2>                  
-	<?php echo $model->description; ?>
-              
-</div>
